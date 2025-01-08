34f216a6b16f30feb7fc151ae944ce932133f903
znxdg <102642636+znxdg@users.noreply.github.com>
2025-01-08 20:53:11 +0800

    Update 認識四大常用資料類型

--- Assets/Scripts/Class_1_Variable.cs
+++ Assets/Scripts/Class_1_Variable.cs
@@ -8,16 +8,22 @@
 using UnityEngine;
 
 // 修飾詞 類別關鍵字(腳本) 類別名稱(腳本名稱)
-public class Class_1_Variable
+public class Class_1_Variable:MonoBehaviour
 {
     /*程式內的括號都是成對出現
      * 
      * 變數:會改變的數值，定義一個記憶體空間用來存放資訊
      * 變數語法:
-     *  變數資料類型 變數名稱;
+     *  修飾詞 變數資料類型 變數名稱;
      *  資料類型: 告訴記憶體可以放入那些資料
      *  變數名稱:習慣用小寫開頭命名，規則與腳本名稱相同
+     *  修飾詞：
+     *      公開：允許其他類別存取,顯示在Unity屬性面板上  public
+     *      私人：禁止其他類別存取，隱藏在Unity屬性面板上 private (預設)
      */
-    int coin;
+    public int coin = 500;
+    public float style = 2.3f;
+    private int leavel = 3;
 }
 
+//類別外，程式不寫這
---
+++ Assets/Scripts/Class_2_DataType.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// 三斜線是摘要(XML 語言)用來做簡短說明，可加在類別與變數上方
+/// <summary>
+/// 認識 C# 資料類型
+/// </summary>
+public class Class_2_DataType : MonoBehaviour
+{
+    /*變數語法：
+     *  修飾詞 資料類型 變數名稱 指定 預設值;
+     *四大常用資料類型：
+     *  整數(int)：儲存沒有小數點的數值
+     *  浮點數(float)：儲存有小數點的數值，數值須加上後綴 f
+     *  字串(string)：儲存文字，必須用雙引號
+     *  布林值(bool)：儲存正(true)與反(false)
+     */
+    public int count = 7;
+
+    //float後綴的f 大小寫皆可
+    public float moveSpeed = 3.5f;
+    public float turnSpeed = 30.7F; 
+
+    public string character = "小游飯";
+
+    public bool isDead = false;
+    public bool gameOver = true;
+}
