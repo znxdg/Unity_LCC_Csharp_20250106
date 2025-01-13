b5b0565cd0068021a78a8a24478abec5e90b1dc8
znxdg <102642636+znxdg@users.noreply.github.com>
2025-01-13 19:12:45 +0800

    Update 完成作業練習

---
+++ Assets/Practices/Practices_1_Variable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Practices_1_Variable:MonoBehaviour
+{
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    private bool getDragonSoul = true;
+}
---
+++ Assets/Practices/Practices_2_DataType.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Practices_2_DataType:MonoBehaviour
+{
+    public float variableFloat = 7.7f;
+    public double variableDouble = 0.1235;
+    public decimal variableDecimal = 1.23456789m;
+}
