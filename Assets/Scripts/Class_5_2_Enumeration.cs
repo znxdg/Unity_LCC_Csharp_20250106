94ae73290826ac0cc75302ad2d197be787247c78
znxdg <102642636+znxdg@users.noreply.github.com>
2025-01-22 19:41:23 +0800

    Update 迴圈

---
+++ Assets/Scripts/Class_5_2_Enumeration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace YuCheng
+{
+    /// <summary>
+    /// 列舉：enumeration，C# 內關鍵字為簡寫enum
+    /// </summary>
+    public class Class_5_2_Enumeration : MonoBehaviour
+    {
+        // 列舉：定義下拉選單的選項(通常都是單選)
+        // 語法：
+        // 修飾詞 關鍵字 enum 列舉名稱 { 列舉選項 }
+        // 定義一個列舉，名稱叫做季節
+        // 列舉都是數值，預設從 0 開始
+        private enum Season
+        {
+            Spring, Summer, Autumn, Winter
+        }
+
+        // 宣告變數，類型為季節列舉，預設值為夏天
+        [SerializeField, Header("季節")]
+        private Season season = Season.Summer;
+
+        private void Awake()
+        {
+            // 取得列舉的值 (Get) 抓出值
+            Debug.Log(season);
+            // 取得列舉的整數值
+            Debug.Log((int)season);
+        }
+    }
+}
---
+++ Assets/Scripts/Class_6_1_Iteration.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+namespace YuCheng
+{
+    /// <summary>
+    /// 反覆運算陳述式：迴圈
+    /// for、foreach、do 與 while
+    /// </summary>
+    public class Class_6_1_Iteration:MonoBehaviour
+    {
+        private void Awake()
+        {
+            // 迴圈請在一次性事件(Awake、Start)內使用
+            // while 迴圈：當布林值為 true 時會持續執行
+            // while(布林值) { 程式區塊 }
+            // 無限迴圈：布林值一直是 true
+            // 宣告區域變數 i 為 0
+            int i = 0;
+            //當 i < 5 就執行 {}
+            while (i < 5)
+            {
+                Debug.Log($"<color=#f33>while 迴圈，i：{i}</color>");
+                // i 遞增 ( i + 1)
+                i++;
+            }
+
+            // do 迴圈：
+            // do { 程式區塊 } while (布林值)
+            int j = 0;
+            // j = 5;
+            do
+            {
+                Debug.Log($"<color=#f79>do 迴圈，j：{j}</color>");
+                j++;
+            }
+            while (j < 5);
+
+            // for 迴圈:
+            // for (初始值; 布林值; 迭代器) { 程式區塊 }
+            for (int k = 0; k < 5; k++)
+            {
+                Debug.Log($"<color=#78f>for 迴圈，k：{k}</color>");
+            }
+        }
+    }
+}
