2aa9d89c45a4c6b11c05ea500129b41467145304
znxdg <102642636+znxdg@users.noreply.github.com>
2025-02-09 21:18:22 +0800

    Update 練習5 HW

---
+++ Assets/Practices/Practices_5_Selection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace YuCheng
+{
+    public class Practices_5_Selection:MonoBehaviour
+    {
+        [SerializeField, Header("血量"), Range(0, 100)]
+        private int hp;
+
+        private void Update()
+        {
+            switch (hp)
+            {
+                case >= 80:
+                    Debug.Log("<color=#f3f>血量安全</color>");
+                    break;
+                case >= 60:
+                    Debug.Log("<color=#f3f>健康狀況有狀況</color>");
+                    break;
+                case >= 40:
+                    Debug.Log("<color=#f3f>警告，快喝水</color>");
+                    break;
+                case >= 10:
+                    Debug.Log("<color=#f3f>快死掉了</color>");
+                    break;
+                case 0:
+                    Debug.Log("<color=#f3f>你已經死了</color>");
+                    break;
+            }
+        }
+    }
+}
