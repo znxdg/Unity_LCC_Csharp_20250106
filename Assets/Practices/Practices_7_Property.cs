b27df0efe647748b563b88009884e79e93152797
znxdg <102642636+znxdg@users.noreply.github.com>
2025-02-19 00:54:53 +0800

    Update 練習_7_屬性

---
+++ Assets/Practices/Practices_7_Property.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace YuCheng
+{
+    /// <summary>
+    /// 屬性練習
+    /// </summary>
+    public class Practices_7_Property : MonoBehaviour
+    {
+        [SerializeField]
+        private float _hp = 100;
+
+        public float hp
+        {
+            get
+            {
+                if (_hp <= 0) Debug.Log("<color=#f33>玩家角色已經死亡</color>");
+                return _hp;
+            }
+        }
+
+        private void Update()
+        {
+                Debug.Log(hp);
+        }
+    }
+}
