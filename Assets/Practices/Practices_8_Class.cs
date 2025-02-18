edefa1195e19c9e4855f0c5b29e15750a44b4c95
znxdg <102642636+znxdg@users.noreply.github.com>
2025-02-19 01:13:00 +0800

    Update 練習_8_類別

---
+++ Assets/Practices/Practices_8_Class.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace YuCheng
+{ 
+    /// <summary>
+    /// 練習類別
+    /// </summary>
+    public class Practices_8_Class : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Practices_8_BOSS bossDeagon = new Practices_8_BOSS("龍獸", "龍之吐息", 999);
+            Practices_8_BOSS bossPig = new Practices_8_BOSS("半獸人", "銀斧斬耀", 540);
+            
+            Debug.Log($"<color=#f93>{bossDeagon.name}, 招式：{bossDeagon.skill}</color>");
+            Debug.Log($"<color=#f93>{bossPig.name}, 招式：{bossPig.skill}</color>");
+        }
+    }
+
+    /// <summary>
+    /// 大魔王類別
+    /// </summary>
+    public class Practices_8_BOSS
+    {
+        public string name;
+        public string skill;
+        public float hp;
+
+        public Practices_8_BOSS(string _name, string _skill, float _hp)
+        {
+            name = _name;
+            skill = _skill;
+            hp = _hp;
+        }
+    }
+}
