577ee8fd55863cdd5035fdf6121642af28aef378
znxdg <102642636+znxdg@users.noreply.github.com>
2025-03-12 21:40:59 +0800

    Update 課程16_資料結構

--- Assets/Scripts/Class_15_Generics.cs
+++ Assets/Scripts/Class_15_Generics.cs
@@ -24,7 +24,7 @@ namespace YuCheng.Class_15
             object objA = '嗨', objB = 7.7f;
             LogSystem.LogWithColor($"物件 A 與 B : {objA} | {objB}", "#f93");
             SwapObject(ref objA, ref objB);
-            LogSystem.LogWithColor($"物件 A 與 B : {objA} | {objB}", "#f93"); 
+            LogSystem.LogWithColor($"物件 A 與 B : {objA} | {objB}", "#f93");
             #endregion
 
             bool boolA = true, boolB = false;
@@ -94,10 +94,28 @@ namespace YuCheng.Class_15
             T temp = a;
             a = b;
             b = temp;
-        } 
+        }
         #endregion
+
+        private void Start()
+        {
+            var player = new Player();
+            var enemy = new Enemy();
+            var attackEvent = new AttackEvent<Player, Enemy>();
+            attackEvent.Attack(player, enemy);
+
+            var hp = new Hp();
+            var attack = new Attack();
+            hp.Increase(10.5f);
+            attack.Increase(50);
+            hp.Increase(3.75f);
+
+            var checker = new CheckValue<Hp, float>();
+            checker.Check(hp);
+        }
     }
 
+    #region 泛型類別
     public class DataPlayer<T>
     {
         public T data;
@@ -105,6 +123,60 @@ namespace YuCheng.Class_15
         public void LogData(T data)
         {
             LogSystem.LogWithColor(data, "#3ff");
+        }
+    }
+
+    public class Player { }
+    public class Enemy { }
+
+    public class AttackEvent<T, U>
+    {
+        public void Attack(T attacker, U defender)
+        {
+            LogSystem.LogWithColor($"{attacker} 攻擊 {defender}", "#f3f");
+        }
+    }
+    #endregion
+
+
+    #region 泛型介面
+    // 泛型介面
+    public interface IStat<T>
+    {
+        public T value { get; set; }        // 該狀態的值
+        public void Increase(T amount);     // 增加該狀態
+    }
+
+    public class Hp : IStat<float>
+    {
+        public float value { get; set; }
+        public void Increase(float amount)
+        {
+            value += amount;
+            LogSystem.LogWithColor($"血量:{value}", "#f3f");
+        }
+    }
+
+    public class Attack : IStat<int>
+    {
+        public int value { get; set; }
+
+        public void Increase(int amount)
+        {
+            value += amount;
+            LogSystem.LogWithColor($"血量:{value}", "#f3f");
+        }
+    } 
+    #endregion
+
+
+    // 泛型約束：泛型 T 必須實作 IStat<U> 介面，才會執行該類別
+    public class CheckValue<T, U> where T : IStat<U>
+    {
+        public void Check(T stat)
+        {
+            // 添加約束後可以使用 IStat<T> 成員
+            LogSystem.LogWithColor($"狀態的值：{stat.value}", "#3d3");
         }
     }
 }
---
+++ Assets/Scripts/Class_16_DataStruct.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using YuCheng.Tool;
+
+namespace YuCheng.Class_16
+{
+    /// <summary>
+    /// 資料結構
+    /// </summary>
+    public class Class_16_DataStruct:MonoBehaviour
+    {
+        #region 清單
+        // 陣列
+        public int[] attacks = { 10, 15, 7 };
+        public float[] defens = { 1.5f, 7.5f, 0.3f };
+
+        // 清單：泛型集合
+        public List<int> speeds = new List<int>() { 3, 9, 7 };
+        public List<string> props = new List<string>() { "藥水", "地圖" };
+        public List<float> playerDefens; 
+        #endregion
+
+        private void Awake()
+        {
+            #region 清單
+            // 清單存取：與陣列相同
+            LogSystem.LogWithColor($"第三筆速度：{speeds[2]}", "#3f3");
+            speeds[0] = 10;
+            LogSystem.LogWithColor($"第一筆速度：{speeds[0]}", "#3f3");
+            // 陣列初始化後就無法增減長度
+            // 添加一筆道具
+            props.Add("寶劍");
+            // 刪除第一筆道具
+            props.RemoveAt(0);
+            // 添加頭盔到編號 1 上
+            props.Insert(1, "頭盔");
+
+            foreach (var prop in props)
+            {
+                LogSystem.LogWithColor($"道具：{prop}", "#f93");
+            }
+
+            // 使用建構子帶入陣列
+            playerDefens = new List<float>(defens);
+            // 排序：由小到大
+            playerDefens.Sort();
+            // 反排序：由大到小
+            playerDefens.Reverse();
+
+            foreach (var item in playerDefens)
+            {
+                LogSystem.LogWithColor($"資料：{item}", "#f93");
+            }
+
+            // Count 指清單內的資料，根據 Add 或 Remove 改變
+            LogSystem.LogWithColor($"數量：{props.Count}", "#77f");
+            // Capcity 指清單內的容量，系統自動分配，預設為 4 並且以倍數成長 (C# 各版本不同)
+            LogSystem.LogWithColor($"容量：{props.Capacity}", "#77f");
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(i);
+                LogSystem.LogWithColor($"數量：{numbers.Count}", "#3a3");
+                LogSystem.LogWithColor($"數量：{numbers.Capacity}", "#f39");
+            }
+            #endregion
+
+            #region 堆疊
+            // 堆疊：先進後出，類似椅子堆在一起
+            Stack<string> enemys = new Stack<string>();
+            // 放資料進入堆疊
+            enemys.Push("史萊姆");
+            enemys.Push("哥布林");
+            LogStack<string>(enemys);
+            // 拿資料並且不移除
+            enemys.Peek();
+            LogStack<string>(enemys);
+            // 拿資料並移除
+            enemys.Pop();
+            LogStack<string>(enemys);
+            // 判斷是否包含某筆資料
+            LogSystem.LogWithColor($"{enemys.Contains("哥布林")}", "#3f6");
+            // 清除所有資料
+            enemys.Clear();
+            LogStack<string>(enemys); 
+            #endregion
+
+            Queue<string> player = new Queue<string>();
+            player.Enqueue("盜賊");
+            player.Enqueue("法師");
+            player.Enqueue("戰士");
+            LogQueue<string>(player);
+            // 拿東西不刪除，與堆疊的 Peek 相同
+            LogSystem.LogWithColor(player.Peek(), "#f33");
+            LogQueue<string>(player);
+            // 拿東西並刪除，與堆疊的 Pop 相同
+            LogSystem.LogWithColor(player.Dequeue(), "#f33");
+            LogQueue<string>(player);
+        }
+
+        private void LogStack<T>(Stack<T> stack)
+        {
+            LogSystem.LogWithColor("----------", "#fff");
+            foreach (var item in stack)
+            {
+                LogSystem.LogWithColor($"堆疊資料：{item}", "#f77");
+            }
+        }
+
+        private void LogQueue<T>(Queue<T> queue)
+        {
+            LogSystem.LogWithColor("----------", "#fff");
+            foreach (var item in queue)
+            {
+                LogSystem.LogWithColor($"佇列資料：{item}", "#7f7");
+            }
+        }
+    }
+}
